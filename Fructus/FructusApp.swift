9ecb4f2bec00a05b571571591c23f54ffa28a711
SIddharth SIngh <siddharth.singh2019@vitstudent.ac.in>
2020-12-04 10:51:23 +0530

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Siddharth Singh on 04/12/20.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
